c212c6cac2cebcc0f1a656433c36e3f1af7c6bf7
edusousa78 <sousa.ej@gmail.com>
2024-09-10 09:09:10 -0300

    atualizando...

--- src/ResultadoEscolar.java
+++ src/ResultadoEscolar.java
@@ -1,5 +1,13 @@
 public class ResultadoEscolar {
 public static void main(String[] args) {
+
+    int nota = 9;
+
+    if(nota >= 7){
+        System.out.println("Aluno Aprovado, com a nota " + nota + "!");
+    }else{
+        System.out.println("Aluno Reprovado, com a nota " + nota + "!");
+    }
     
 }
 }
