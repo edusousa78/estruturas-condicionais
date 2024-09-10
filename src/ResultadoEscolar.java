30a1ca8ca4eafc8996c77cc8afbaf0cc7edbdad8
edusousa78 <sousa.ej@gmail.com>
2024-09-10 09:22:57 -0300

    atualização do Código

--- src/ResultadoEscolar.java
+++ src/ResultadoEscolar.java
@@ -1,11 +1,16 @@
 public class ResultadoEscolar {
 public static void main(String[] args) {
-
-    int nota = 9;
+    
+//condição simple, composta e encadeada
+    int nota = 8;
 
     if(nota >= 7){
         System.out.println("Aluno Aprovado, com a nota " + nota + "!");
-    }else{
+    } else if(nota > 5 && nota < 7){
+
+        System.out.println("Prova de Recuperação");
+
+    } else {
         System.out.println("Aluno Reprovado, com a nota " + nota + "!");
     }
     
